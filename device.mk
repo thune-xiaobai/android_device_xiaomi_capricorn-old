466313a862dbfa39740008f6f6dddf313d05befc
pocarropa <yusjota@yahoo.es>
2017-03-31 17:01:38 +0200

    Fix fingerprint location-remove unusued services

--- device.mk
+++ device.mk
@@ -120,8 +120,7 @@ PRODUCT_PACKAGES += \
 
 # Device config scripts
 PRODUCT_PACKAGES += \
-    init.leds.sh \
-    init.qcom.bt.sh
+    init.leds.sh 
 
 # Device init scripts
 PRODUCT_PACKAGES += \
@@ -201,10 +200,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes \
     libebtc
-
-# IRQ Balancer
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -299,5 +294,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
